4f7f6a99de1fb129201d86c2a700bbec8e212e82
Marlee Eyre <marlee.eyre@gmail.com>
2020-12-17 21:31:03 -0500

    schema.sql file created, database structure started

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE git_interviewDB;
+USE git_interviewDB;
+
+CREATE TABLE question
+(
+    id
+    author_id
+)
+
+CREATE TABLE solutions
+
+CREATE TABLE author
+(
+    author_id
+
+)
+
+CREATE TABLE user
+(
+    id 
+    username
+    email
+    password
+)
+
