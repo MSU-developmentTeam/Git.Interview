a3cf6f92b87382c49a98b1a49a01f1a6170b3095
progg1992 <roggpierce@gmail.com>
2021-01-03 19:36:31 -0600

    trying to get questions to display from the database

--- routes/questions-api-routes.js
+++ routes/questions-api-routes.js
@@ -10,17 +10,27 @@ module.exports = function (app) {
     db.Question.create({
       UserId: req.user.id,
       topic: req.body.topic,
-      body: req.body.body
+      body: req.body.body,
+      answer: req.body.answer
     }).then(function(){
       res.redirect("/profile");
     });
   });
 
-  app.get("/questions", function (req, res) {
+  app.get("/api/questions", function (req, res) {
     const query = {};
+    console.log(req.query.username)
     if (req.query.username) {
+      console.log('Inside IF')
       query.Username = req.query.username;
+      db.Question.findAll({
+        where: query,
+        include: [db.User]
+      }).then(function (dbQuestion) {
+        res.json(dbQuestion);
+      });
     }
+    console.log('Outside IF')
     // Finds all questions from all authors
     db.Question.findAll({
       where: query,
@@ -29,30 +39,31 @@ module.exports = function (app) {
       res.json(dbQuestion);
     });
   });
-
+  // Search form
   app.get('/questions/search', function(req, res) {
-    console.log('This Far ...');
-    console.log(req.query)
+    // console.log('This Far ...');
+    // console.log(req.query)
     let { topic } = req.query;
     console.log(topic);
     
     db.Question.findAll({ 
     where: { topic: { [Op.like]: topic } },
-    include: [db.Question && db.User]
+    include: [db.User]
     }).then(function(topic) {
-      console.log(topic)
+      //console.log(topic)
       res.render('searchResults', { topic })
+    }).catch(function(err) {
+      console.log(err);
     });
   });
 
-//   app.get("/api/questions/:topic", function (req, res) {
-//     db.Question.findAll({
-//       where: {
-//         topic: req.params.topic
-//       },
-//       include: [db.User]
-//     }).then(function (dbQuestion) {
-//       res.json(dbQuestion);
-//     });
-//   });
+  // app.get("questions/:topic", function (req, res) {
+  //   db.Question.findAll({
+  //     where: { topic: { [Op.like]: topic } },
+  //   include: [db.Question && db.User]
+  //   }).then(function(topic) {
+  //     console.log(topic)
+  //     res.render('searchResults', { topic })
+  //   });
+  // });
 };
